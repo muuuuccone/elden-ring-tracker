7a2a1cd5ea161e854f8e11caf48b6865a823fcce
Gianmarco Capasso <gianmarco.capasso@gmail.com>
2024-06-17 09:27:02 +0200

    feat: added margin to filter

--- src/components/zone_tabs/index.tsx
+++ src/components/zone_tabs/index.tsx
@@ -5,12 +5,11 @@ import {getSubZones} from "@/utils/functions/getZones";
 import {useRouter, useSearchParams} from "next/navigation";
 import ItemsGrid from "@/components/items_grid";
 import useSaveData from "@/hooks/useSaveData";
-import Typography from "@mui/material/Typography";
 import {Check} from "@mui/icons-material";
 import styles from './index.module.css';
 import FilterTab from "@/components/filter_tab";
 
-export default function ZoneTabs(){
+export default function ZoneTabs() {
     const {inventory} = useSaveData();
     const [value, setValue] = useState('')
     const router = useRouter();
@@ -27,13 +26,13 @@ export default function ZoneTabs(){
 
     let subZones: SubZone[] = []
 
-    if(zone !== 'All Items') subZones = getSubZones(zone);
+    if (zone !== 'All Items') subZones = getSubZones(zone);
 
     subZones = subZones.filter((subZone) => Object.keys(subZone.items).length > 0)
 
     const completionStatus = (zone: string) => {
         const zoneData = subZones.find((subZone) => subZone.name === zone);
-        if(!zoneData) return '';
+        if (!zoneData) return '';
         const totalItems = Object.keys(zoneData.items).length;
         const collectedItems = Object.keys(zoneData.items).filter((item) => inventory.includes(item)).length;
 
@@ -44,26 +43,29 @@ export default function ZoneTabs(){
         )
     }
 
-    return(
+    return (
         <Box sx={{minWidth: 400}}>
-            {subZones.length > 0 && <FormControl fullWidth>
-                <InputLabel>{zone === 'Quests' ? "Select the quest" : "Select the zone"}:</InputLabel>
-                <Select
-                    label='Select the zone:'
-                    labelId='zone-select-label'
-                    id='zone-select'
-                    value={value}
-                    onChange={handleChange}
-                >
-                    {subZones.map((zone) => (
-                        <MenuItem value={zone.name} key={zone.name}>
-                            <Box className={styles.menuItem}>
-                                {completionStatus(zone.name)}{zone.name}
-                            </Box>
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>}
+            {subZones.length > 0 &&
+                <Box className={styles.select}>
+                    <FormControl fullWidth>
+                        <InputLabel>{zone === 'Quests' ? "Select the quest" : "Select the zone"}:</InputLabel>
+                        <Select
+                            label='Select the zone:'
+                            labelId='zone-select-label'
+                            id='zone-select'
+                            value={value}
+                            onChange={handleChange}
+                        >
+                            {subZones.map((zone) => (
+                                <MenuItem value={zone.name} key={zone.name}>
+                                    <Box className={styles.menuItem}>
+                                        {completionStatus(zone.name)}{zone.name}
+                                    </Box>
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>}
             <FilterTab/>
             <ItemsGrid/>
         </Box>
